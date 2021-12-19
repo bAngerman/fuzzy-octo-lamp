b7da0397b9acfa8d05544182b1f45eb31abdf990
bAngerman <brendan.angerman@gmail.com>
2021-12-19 11:22:24 -0700

    krangled day14 :(

--- day12/main.go
+++ day12/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"log"
 	"strings"
 	"unicode"
 
@@ -15,8 +14,7 @@ type room struct {
 	isBigRoom bool
 
 	// Pointer for next room idx(s) and previous room idx
-	prevRoom  int
-	nextRooms []int
+	nextRooms []string
 }
 
 func IsUpper(s string) bool {
@@ -38,66 +36,21 @@ func findRoomByName(rooms []room, name string) (int, error) {
 	return 0, errors.New("Unable to find room with name " + name)
 }
 
-func pathfind(roomCopy []room, curRoomIdx, pathCount int) int {
-
-	// curRoom := roomCopy[curRoomIdx]
-	// prevRoom := roomCopy[curRoom.prevRoom]
-
-	// pretty.Println("Currently in", curRoom.name)
-
-	// // Room has been visited already, and it's a small room (only 1 visit allowed)
-	// // if curRoom.visited && !curRoom.isBigRoom {
-	// // 	pretty.Println("We have already visited ", curRoom.name, "so this path is invalid.")
-
-	// // 	return 0
-	// // }
-
-	// if len(curRoom.nextRooms) != 0 {
-	// 	for _, nextRoomIdx := range curRoom.nextRooms {
-	// 		// nextRoom := roomCopy[nextRoomIdx]
-
-	// 		// return pathfind(roomCopy, nextRoomIdx, pathCount)
-	// 		// if len(nextRoom.nextRooms) != 0 {
-	// 		// } else {
-
-	// 		// 	pretty.Println("There are no rooms after the next room", nextRoom.name)
-	// 		// }
-	// 	}
-	// } else {
-	// 	pretty.Println("There are no next rooms connected to", curRoom.name)
-	// 	// Made it to the end, incrementing path count.
-	// 	if curRoom.name == "end" {
-	// 		pretty.Println("made it to end")
-	// 		return pathCount + 1
-	// 	}
-
-	// 	// if !prevRoom.isBigRoom {
-	// 	// 	pretty.Println("The previous room is")
-	// 	// 	return 0
-	// 	// }
-	// }
-
-	return pathCount
-}
-
 func part1(rooms []room) {
-	var distinctPathCount int
+	var pathCount int
+	pretty.Println(rooms)
 
-	pretty.Println("Rooms", rooms)
+	/**
+	 * Starting at the "start" room, start building arrays of
+	 * room names (paths) by going through each next connected room.
+	 * Need to do some recursion to continue finding next rooms.
+	 */
 
-	startRoomIdx, err := findRoomByName(rooms, "start")
-	if err != nil {
-		log.Panic(err)
+	start, _ := findRoomByName(rooms, "start")
+
+	for _, nextRoom := range rooms[start].nextRooms {
 	}
 
-	startRoom := rooms[startRoomIdx]
-	for _, roomIdx := range startRoom.nextRooms {
-		roomCopy := rooms
-		distinctPathCount = distinctPathCount + pathfind(roomCopy, roomIdx, 0)
-	}
-
-	// pretty.Println(rooms)
-	pretty.Println("Count of distinct paths", distinctPathCount)
 }
 
 func part2(rooms []room) {
@@ -144,21 +97,15 @@ func main() {
 		for roomIdx, room := range rooms {
 			if room.name == parts[0] {
 
-				idx := 0
-				found := false
+				name := ""
 
-				for nextRoomIdx, nextRoom := range rooms {
+				for _, nextRoom := range rooms {
 					if nextRoom.name == parts[1] {
-						idx = nextRoomIdx
-						found = true
+						name = nextRoom.name
 					}
 				}
 
-				if !found {
-					panic("Something went wrong when finding the next room index Sadgeg")
-				}
-
-				rooms[roomIdx].nextRooms = append(rooms[roomIdx].nextRooms, idx)
+				rooms[roomIdx].nextRooms = append(rooms[roomIdx].nextRooms, name)
 			}
 		}
 	}
--- day14/main.go
+++ day14/main.go
@@ -1,18 +1,261 @@
 package main
 
-import "github.com/bAngerman/adventofcode/utils"
+import (
+	"log"
+	"math"
+	"sort"
+	"strings"
 
-func part1(items []int) {
+	"github.com/bAngerman/adventofcode/utils"
+)
 
+type pairInsert struct {
+	pattern string
+	val     string
 }
 
-func part2(items []int) {
+type insert struct {
+	val string
+	idx int
+}
 
+func applyPolymerTemplate(tmpl []string, pairInserts []pairInsert) []string {
+	var inserts []insert
+
+	for _, item := range pairInserts {
+		itemPat := strings.Split(item.pattern, "")
+
+		// The commands to run after which tell us where to insert characters.
+
+		// Reset the index of the inner loop.
+		idx := 0
+		for {
+			if idx > len(tmpl) {
+				break
+			}
+
+			if idx == len(tmpl)-1 {
+				// No possibility to match 2 chars.
+				break
+			}
+
+			if itemPat[0] == tmpl[idx] && itemPat[1] == tmpl[idx+1] {
+				inserts = append(inserts, insert{val: item.val, idx: idx + 1})
+				// log.Println("Insert found", item.val, "at index", idx+1)
+			}
+
+			idx++
+		}
+	}
+
+	// We need insertIndex because every time we add an element, we need to offset our initial calculation for offset.
+	insertCount := 0
+
+	// Make sure we sort the inserts in ascending order so we insert correctly.
+	sort.Slice(inserts, func(i, j int) bool {
+		return inserts[i].idx < inserts[j].idx
+	})
+
+	for _, insert := range inserts {
+		// log.Println(insert)
+		idx := insert.idx + insertCount
+
+		// log.Println("Inserting at", idx, tmpl)
+
+		tmpl = append(tmpl[:idx+1], tmpl[idx:]...)
+
+		tmpl[idx] = insert.val
+
+		// log.Println("After insert", tmpl)
+
+		insertCount++
+	}
+
+	return tmpl
+}
+
+// Brute force solution.
+func part1(tmpl []string, pairInserts []pairInsert) {
+
+	steps := 10
+	for i := 0; i < steps; i++ {
+		tmpl = applyPolymerTemplate(tmpl, pairInserts)
+
+		// log.Println("Step", i+1)
+		// log.Println(tmpl)
+		// log.Println()
+	}
+
+	var counts = make(map[string]int)
+	for _, tmplItem := range tmpl {
+
+		_, ok := counts[tmplItem]
+
+		if ok {
+			counts[tmplItem] = counts[tmplItem] + 1
+		} else {
+			counts[tmplItem] = 1
+		}
+	}
+
+	mostCommon := insert{}
+	leastCommon := insert{idx: math.MaxInt}
+
+	for letter, count := range counts {
+		if mostCommon.idx < count {
+			mostCommon = insert{idx: count, val: letter}
+		}
+
+		if leastCommon.idx > count {
+			leastCommon = insert{idx: count, val: letter}
+		}
+	}
+
+	log.Println("Counts", counts)
+	log.Println("Diff", mostCommon.idx-leastCommon.idx)
+}
+
+// Keep track of pairs in a map containing counts of pairs.
+// map[CB:1 NC:1 NN:1]
+// TODO: There is a bug with the left and right neighbours incrememnting pair values as a result of inserts..
+func part2(tmpl []string, pairInserts []pairInsert) {
+	pairs := make(map[string]int)
+
+	idx := 0
+	for {
+		if idx > len(tmpl) {
+			break
+		}
+
+		if idx == len(tmpl)-1 {
+			// No possibility to match 2 chars.
+			break
+		}
+
+		pairString := tmpl[idx] + tmpl[idx+1]
+
+		pairs[pairString] = 1
+
+		idx++
+	}
+
+	log.Println("Pairs before", pairs)
+
+	steps := 10
+	for i := 0; i < steps; i++ {
+		toInsert := make(map[string]int)
+
+		for _, insert := range pairInserts {
+
+			pairVal, pairExists := pairs[insert.pattern]
+
+			if pairExists {
+
+				if pairVal == 0 {
+					// Skip, there are no occurances.
+					continue
+				}
+
+				// log.Println("This pattern exists", insert.pattern, "should insert", insert.val)
+
+				// Add new patterns with each end of the pattern to the map.
+				// Or increment their count if they exist in the map already.
+				// Decrement this pairs[insert.pattern] count
+				parts := strings.Split(insert.pattern, "")
+
+				firstNewPair := parts[0] + insert.val
+				secondNewPair := insert.val + parts[1]
+
+				// log.Println("First", firstNewPair)
+				// log.Println("Second", secondNewPair)
+
+				// log.Println("new pairs are", firstNewPair, "second pair is", secondNewPair)
+
+				toInsert[firstNewPair] = pairs[insert.pattern]
+				toInsert[secondNewPair] = pairs[insert.pattern]
+
+				_, patternExists := toInsert[insert.pattern]
+
+				if patternExists {
+					toInsert[insert.pattern] = toInsert[insert.pattern] - pairs[insert.pattern]
+				} else {
+					toInsert[insert.pattern] = -1 * pairs[insert.pattern]
+				}
+
+				// log.Println("To Insert", toInsert)
+			}
+
+		}
+
+		// log.Println("To Insert", toInsert)
+		// log.Println("Before insert pairs", pairs)
+
+		for insertVal, count := range toInsert {
+			_, exists := pairs[insertVal]
+			if exists {
+				pairs[insertVal] = pairs[insertVal] + count
+				if pairs[insertVal] < 0 {
+					pairs[insertVal] = 0
+				}
+			} else {
+				pairs[insertVal] = count
+
+				if pairs[insertVal] < 0 {
+					pairs[insertVal] = 0
+				}
+			}
+		}
+
+		log.Println("After step", i+1)
+		log.Println("Pairs", pairs)
+
+		log.Println()
+		letterCounts := make(map[string]int)
+
+		for val, count := range pairs {
+			// log.Println("Value", val, "has count", count)
+			letters := strings.Split(val, "")
+
+			for _, letter := range letters {
+				_, exists := letterCounts[letter]
+
+				if exists {
+					letterCounts[letter] = letterCounts[letter] + count
+				} else {
+					letterCounts[letter] = count
+				}
+			}
+		}
+
+		// We doubled counted values because pairs shared values.
+		for letter, count := range letterCounts {
+			letterCounts[letter] = int(math.Ceil(float64(count) / float64(2)))
+		}
+
+		log.Println("Letter counts", letterCounts)
+	}
 }
 
 func main() {
-	inputArr, _ := utils.ReadNumberFile("day14/input")
+	inputArr, _ := utils.ReadTextFile("day14/input2")
 
-	// part1(inputArr)
-	// part2(inputArr)
+	var tmpl []string
+	var inserts []pairInsert
+
+	for idx, val := range inputArr {
+		if val == "" {
+			continue
+		}
+
+		if idx == 0 {
+			// Polymer template
+			tmpl = strings.Split(val, "")
+		} else {
+			parts := strings.Split(strings.ReplaceAll(val, " ", ""), "->")
+			inserts = append(inserts, pairInsert{pattern: parts[0], val: parts[1]})
+		}
+	}
+
+	// part1(tmpl, inserts)
+	part2(tmpl, inserts)
 }
