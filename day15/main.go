aa52b8e01eb47405fcdf5e36e47432183ea68034
bAngerman <brendan.angerman@gmail.com>
2021-12-20 12:39:17 -0700

    day17 brute force

--- day15/main.go
+++ day15/main.go
@@ -1,18 +1,100 @@
 package main
 
-import "github.com/bAngerman/adventofcode/utils"
+import (
+	"strconv"
+	"strings"
 
-func part1(items []int) {
+	"github.com/bAngerman/adventofcode/utils"
+)
+
+type route struct {
+	points  []point
+	current point
+	risk    int
+}
+
+type point struct {
+	x int
+	y int
+}
+
+func traverse(routes []route, cave [][]int) []route {
+	// maxX := len(cave[0])
+	// maxY := len(cave)
 
 }
 
-func part2(items []int) {
+func part1(cave [][]int) {
+	/*
+		We start at 0,0. There is only two positions to travel to x+1 (1,0), y+1 (0,1)
+		Each position then has two options to traverse to. (left, right)
+	*/
+
+	routes := []route{
+		route{
+			points: []point{
+				point{
+					x: 0, y: 0,
+				},
+			},
+			current: point{x: 0, y: 0},
+			risk:    0,
+		},
+	}
+
+	routes = traverse(routes, cave)
+}
+
+func part2(cave [][]int) {
 
 }
+
+// func renderPath(cave [][]int, routes []route) {
+// 	render := ""
+// 	for colIdx, col := range cave {
+// 		curRowString := ""
+// 		for rowIdx, item := range col {
+// 			found := false
+// 			for _, point := range routes.points {
+// 				if point.y == colIdx && point.x == rowIdx {
+// 					found = true
+// 				}
+// 			}
+// 			if found {
+// 				curRowString = curRowString + "+"
+// 			} else {
+// 				curRowString = curRowString + fmt.Sprint(item)
+// 			}
+// 			curRowString = curRowString + "\t"
+// 		}
+// 		render = render + "\n" + curRowString
+// 	}
+// 	pretty.Println(render)
+// }
 
 func main() {
-	inputArr, _ := utils.ReadNumberFile("day15/input")
+	var cave [][]int
+	inputArr, _ := utils.ReadTextFile("day15/input2")
 
-	// part1(inputArr)
+	for _, rowStr := range inputArr {
+		rowSplit := strings.Split(rowStr, "")
+		var row []int
+		for _, val := range rowSplit {
+			intVal, _ := strconv.Atoi(val)
+
+			row = append(row, intVal)
+		}
+		cave = append(cave, row)
+	}
+
+	// path := []path{
+	// 	path{x: 0, y: 0},
+	// 	path{x: 0, y: 1},
+	// 	path{x: 0, y: 2},
+	// }
+
+	// renderPath(cave, path)
+
+	part1(cave)
 	// part2(inputArr)
 }
--- day17/main.go
+++ day17/main.go
@@ -1,18 +1,296 @@
 package main
 
-import "github.com/bAngerman/adventofcode/utils"
+import (
+	"log"
 
-func part1(items []int) {
+	"github.com/kr/pretty"
+)
 
+type point struct {
+	x  int
+	y  int
+	xV int
+	yV int
 }
 
-func part2(items []int) {
+func abs(i int) int {
+	if i < 0 {
+		return -i
+	}
 
+	return i
+}
+
+// target area: x=56..76, y=-162..-134
+// test target area: x=20..30, y=-10..-5
+func part1() {
+
+	// minX := 20
+	// maxX := 30
+	// minY := -5
+	// maxY := -10
+
+	minX := 56
+	maxX := 76
+	minY := -134
+	maxY := -162
+
+	log.Println("Target area is", "x", minX, maxX, "y", minY, maxY)
+
+	var xVelocities, yVelocities []int
+
+	for i := 1; i <= maxX; i++ {
+		xVelocities = append(xVelocities, i)
+	}
+
+	for i := maxY * 10; i < abs(minY)*10; i++ {
+		yVelocities = append(yVelocities, i)
+	}
+
+	// pretty.Println(xVelocities, yVelocities)
+
+	var hits []point
+
+	// xVelocities = []int{6}
+	// yVelocities = []int{3}
+
+	for _, x := range xVelocities {
+		for _, y := range yVelocities {
+
+			var xPos, yPos int
+
+			xV := x
+			yV := y
+
+			for {
+
+				// If we are to the right of the target, iterate.
+				// If we are below the target, iterate.
+				// Or if we are not moving, and not directly over the zone.
+				if xPos > maxX || yPos < maxY || xPos < minX && xV == 0 {
+					// log.Println("First", xPos > maxX)
+					// log.Println("Second", yPos < maxY)
+					// log.Println("Third", xPos < minX && xV == 0)
+
+					// log.Println("x", xPos, "y", yPos, "xV", xV, "yV", yV)
+					// log.Println(minX, maxX, minY, maxY)
+					// log.Println()
+
+					break
+				}
+
+				/**
+				Calculate the new position as a result of the velocity.
+				The probe's x position increases by its x velocity.
+				The probe's y position increases by its y velocity.
+
+				THEN check if it is within the target zone.
+				*/
+
+				// log.Println(xPos >= minX)
+				// log.Println(xPos <= maxX)
+
+				// log.Println(yPos >= minY)
+				// log.Println(yPos <= maxY)
+
+				// log.Println()
+
+				// If x pos within minX, maxX and y pos within minY, maxY
+				if xPos >= minX && xPos <= maxX && yPos <= minY && yPos >= maxY {
+					// log.Println("HIT. Position is", xPos, yPos)
+					// log.Println("velocities are", xV, yV)
+					// log.Println()
+					hits = append(hits, point{x: xPos, y: yPos, xV: x, yV: y})
+				}
+
+				xPos = xPos + xV
+				yPos = yPos + yV
+
+				/**
+				Apply the acceleration to the velocity for the next iteration of time.
+				Due to drag, the probe's x velocity changes by 1 toward the value 0; that is,
+				it decreases by 1 if it is greater than 0, increases by 1 if it is less than
+				0,or does not change if it is already 0.
+				Due to gravity, the probe's y velocity decreases by 1.
+				*/
+
+				if xV > 0 {
+					// Decrement by 1 due to drag.
+					xV = xV - 1
+				}
+
+				yV = yV - 1 // Apply gravity
+			}
+		}
+	}
+
+	// These are all the acceleration sets that hit the area.
+	// Now run the kinematic model on these guys to evaluate max height
+	maxHeight := 0
+	maxHit := point{}
+
+	// pretty.Println(hits)
+
+	for _, hit := range hits {
+		var xPos, yPos int
+
+		xV := hit.xV
+		yV := hit.yV
+
+		for {
+			if xPos > maxX || yPos < maxY || xPos < minX && xV == 0 {
+				break
+			}
+
+			xPos = xPos + xV
+			yPos = yPos + yV
+
+			// log.Println(maxHeight, yPos)
+
+			if maxHeight < yPos {
+				maxHeight = yPos
+				maxHit = hit
+			}
+
+			if xV > 0 {
+				xV = xV - 1
+			}
+
+			yV = yV - 1
+		}
+	}
+
+	pretty.Println("Best hit was", maxHit, "with a height of", maxHeight)
+}
+
+// target area: x=56..76, y=-162..-134
+// test target area: x=20..30, y=-10..-5
+func part2() {
+	// minX := 20
+	// maxX := 30
+	// minY := -5
+	// maxY := -10
+
+	minX := 56
+	maxX := 76
+	minY := -134
+	maxY := -162
+
+	// log.Println("Target area is", "x", minX, maxX, "y", minY, maxY)
+
+	var xVelocities, yVelocities []int
+
+	for i := 1; i <= maxX; i++ {
+		xVelocities = append(xVelocities, i)
+	}
+
+	for i := maxY * 10; i < abs(minY)*10; i++ {
+		yVelocities = append(yVelocities, i)
+	}
+
+	// pretty.Println(xVelocities, yVelocities)
+
+	var hits []point
+
+	// xVelocities = []int{6}
+	// yVelocities = []int{3}
+
+	for _, x := range xVelocities {
+		for _, y := range yVelocities {
+
+			var xPos, yPos int
+
+			xV := x
+			yV := y
+
+			for {
+
+				// If we are to the right of the target, iterate.
+				// If we are below the target, iterate.
+				// Or if we are not moving, and not directly over the zone.
+				if xPos > maxX || yPos < maxY || xPos < minX && xV == 0 {
+					// log.Println("First", xPos > maxX)
+					// log.Println("Second", yPos < maxY)
+					// log.Println("Third", xPos < minX && xV == 0)
+
+					// log.Println("x", xPos, "y", yPos, "xV", xV, "yV", yV)
+					// log.Println(minX, maxX, minY, maxY)
+					// log.Println()
+
+					break
+				}
+
+				/**
+				Calculate the new position as a result of the velocity.
+				The probe's x position increases by its x velocity.
+				The probe's y position increases by its y velocity.
+
+				THEN check if it is within the target zone.
+				*/
+
+				// log.Println(xPos >= minX)
+				// log.Println(xPos <= maxX)
+
+				// log.Println(yPos >= minY)
+				// log.Println(yPos <= maxY)
+
+				// log.Println()
+
+				// If x pos within minX, maxX and y pos within minY, maxY
+				if xPos >= minX && xPos <= maxX && yPos <= minY && yPos >= maxY {
+					// log.Println("HIT. Position is", xPos, yPos)
+					// log.Println("velocities are", xV, yV)
+					// log.Println()
+					hits = append(hits, point{x: xPos, y: yPos, xV: x, yV: y})
+				}
+
+				xPos = xPos + xV
+				yPos = yPos + yV
+
+				/**
+				Apply the acceleration to the velocity for the next iteration of time.
+				Due to drag, the probe's x velocity changes by 1 toward the value 0; that is,
+				it decreases by 1 if it is greater than 0, increases by 1 if it is less than
+				0,or does not change if it is already 0.
+				Due to gravity, the probe's y velocity decreases by 1.
+				*/
+
+				if xV > 0 {
+					// Decrement by 1 due to drag.
+					xV = xV - 1
+				}
+
+				yV = yV - 1 // Apply gravity
+			}
+		}
+	}
+
+	// pretty.Println(hits)
+
+	distinct := []point{}
+
+	for _, hit := range hits {
+
+		found := false
+
+		for _, item := range distinct {
+			if item.xV == hit.xV && item.yV == hit.yV {
+				found = true
+			}
+		}
+
+		if !found {
+			distinct = append(distinct, point{xV: hit.xV, yV: hit.yV})
+		}
+	}
+
+	// pretty.Println(distinct)
+	pretty.Println("Distinct velocity count", len(distinct))
 }
 
 func main() {
-	inputArr, _ := utils.ReadNumberFile("day17/input")
+	// inputArr, _ := utils.ReadNumberFile("day17/input2")
 
-	// part1(inputArr)
-	// part2(inputArr)
+	// part1()
+	part2()
 }
